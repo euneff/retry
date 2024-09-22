cd26d409a0bc5ab7fbc26cc1f9363b94464e2a52
jieun <eunef41@naver.com>
2024-09-22 12:50:38 +0900

    hide pw

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,6 @@
+const secureurl=require('./mong');
 const mongoose=require ('mongoose');
-const mongoURI='mongodb://localhost:27017/notez?readPreference=primary&appname=MongoDB%20Compass&ssl=false'
+const mongoURI=secureurl;
 const connectToMongo=()=>{
     mongoose.connect(mongoURI, ()=>{
         console.log("connected to mongo successfully");
