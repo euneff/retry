c7f33c51d17257567b1f02834a09f05203a39dcb
jieun <eunef41@naver.com>
2024-09-27 01:28:59 +0900

    회원가입 디자인 변경

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -39,27 +39,27 @@ const Signup = (props) => {
   }
   return (
 <div className='mt-3'>
-            <h2>Create an account to use NoteZ</h2>
-          <form onSubmit={handleSubmit}>
+            <h2 style={{textAlign:'center'}}>Create an account to use NoteZ</h2>
+          <form onSubmit={handleSubmit} style={{border:'1px solid #e1e3e5',borderRadius:'12px', width:'600px', backgrounColor:'#fff',padding: '20px', margin:'50px 350px' }}>
       <div className="mb-3">
         <label htmlhtmlfor="name" className="form-label">Name</label>
-        <input type="text" className="form-control" id="name" name="name" aria-describedby="emailHelp" onChange={onChange}/>
+        <input type="text" className="form-control" id="name" name="name" aria-describedby="emailHelp" onChange={onChange} placeholder="이름 입력"/>
       </div>
       <div className="mb-3">
         <label htmlhtmlfor="email" className="form-label">Email address</label>
-        <input type="email" className="form-control" id="email" name="email" aria-describedby="emailHelp" onChange={onChange}/>
+        <input type="email" className="form-control" id="email" name="email" aria-describedby="emailHelp" onChange={onChange} placeholder="이메일 주소 "/>
         <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
       </div>
       <div className="mb-3">
         <label htmlhtmlfor="password" className="form-label">Password</label>
-        <input type="password" className="form-control" id="password" name="password" minLength={5} required onChange={onChange}/>
+        <input type="password" className="form-control" id="password" name="password" minLength={5} required onChange={onChange} placeholder="비밀번호"/>
       </div>
       <div className="mb-3">
         <label htmlhtmlfor="cpassword" className="form-label">Confirm Password</label>
-        <input type="password" className="form-control" id="cpassword" name="cpassword" minLength={5} required onChange={onChange}/>
+        <input type="password" className="form-control" id="cpassword" name="cpassword" minLength={5} required onChange={onChange} placeholder="비밀번호 확인"/>
       </div>
      
-      <button type="submit" className="btn btn-primary">Submit</button>
+      <button type="submit" className="btn btn-primary" style={{backgroundColor:'#CBC9F8',color:'#000000'}}>Submit</button>
     </form>
     </div>
   )
